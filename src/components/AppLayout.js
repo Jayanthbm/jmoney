73b2b2ee7a2c6bbc47f3820559ecf03ab23ce3cd
Jayanthbharadwaj M <jayanthbharadwajm@gmail.com>
2025-04-07 19:26:47 +0530

    Add AppLayout component for structured page layout with loading state

---
+++ src/components/AppLayout.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PageHeader from "./PageHeader";
+import Loading from "./Loading";
+
+const AppLayout = ({ title, children, onRefresh, loading }) => {
+  return (
+    <div className="container">
+      <PageHeader
+        title={title}
+        showRefreshButton={onRefresh !== undefined}
+        onRefresh={onRefresh}
+        disabled={loading}
+      />
+      {loading ? <Loading /> : <>{children}</>}
+    </div>
+  );
+};
+
+export default AppLayout;
