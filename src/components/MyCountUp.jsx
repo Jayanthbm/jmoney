86e43c49f815cafd0350e211294f782f655be437
Jayanthbharadwaj M <jayanthbharadwajm@gmail.com>
2025-04-08 11:22:59 +0530

    Remove unnecessary props from CountUp component in MyCountUp for cleaner code

--- src/components/MyCountUp.jsx
+++ src/components/MyCountUp.jsx
@@ -2,15 +2,7 @@ import CountUp from "react-countup";
 import { formatIndianNumber } from "../utils";
 
 const MyCountUp = ({ end }) => {
-  return (
-    <CountUp
-      end={end}
-      duration={1.5}
-      formattingFn={formatIndianNumber}
-      enableScrollSpy
-      scrollSpyDelay={100}
-    />
-  );
+  return <CountUp end={end} duration={1.5} formattingFn={formatIndianNumber} />;
 };
 
 export default MyCountUp;
