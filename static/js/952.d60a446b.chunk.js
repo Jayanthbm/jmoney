4080c6af9a00dbafb3cd95e6f329a9d52157526c
Jayanthbharadwaj M <jayanthbharadwajm@gmail.com>
2025-04-11 18:45:53 +0530

    Updates

---
+++ static/js/952.d60a446b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkjmoney=self.webpackChunkjmoney||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.d60a446b.chunk.js.map
