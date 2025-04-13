cd4f40f46e0f10f7ba7f772d682d891175937535
Jayanthbharadwaj M <jayanthbharadwajm@gmail.com>
2025-04-13 13:45:27 +0530

    feat: Refactor transaction storage to use user-specific stores

--- src/db.js
+++ src/db.js
@@ -3,9 +3,14 @@
 import { createStore, set, get, del, keys } from "idb-keyval";
 import { getSupabaseUserIdFromLocalStorage } from "./utils";
 
-const txStore = createStore("transactions-db", "transactions-store");
+// Create a function to get a user-specific store
+const getUserStore = () => {
+  const userId = getSupabaseUserIdFromLocalStorage();
+  return createStore(`transactions-db-${userId}`, "transactions-store");
+};
 
 export const storeTransactions = async (transactions) => {
+  const txStore = getUserStore();
   for (const tx of transactions) {
     await set(tx.id, tx, txStore);
   }
@@ -14,12 +19,14 @@ export const storeTransactions = async (transactions) => {
 };
 
 export const getAllTransactions = async () => {
+  const txStore = getUserStore();
   const allKeys = await keys(txStore);
   const allData = await Promise.all(allKeys.map((key) => get(key, txStore)));
   return allData;
 };
 
 export const clearTransactions = async () => {
+  const txStore = getUserStore();
   const allKeys = await keys(txStore);
   await Promise.all(allKeys.map((key) => del(key, txStore)));
 };
