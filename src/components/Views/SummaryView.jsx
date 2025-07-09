ccc52d567391a7d0f29f279b1265b4808a2002fc
Jayanthbharadwaj M <jayanthbharadwajm@gmail.com>
2025-07-09 22:45:51 +0530

    feat(SummaryView): integrate MonthYearSelector and TransactionsMode components for improved transaction management

--- src/components/Views/SummaryView.jsx
+++ src/components/Views/SummaryView.jsx
@@ -3,23 +3,20 @@
 import { FaChartBar, FaEyeSlash } from "react-icons/fa";
 import React, { useEffect, useState } from "react";
 import {
-  formatDateToDayMonthYear,
-  formatIndianNumber,
   getMonthOptions,
   getTopCategoryColors,
-  getYearOptions,
   groupAndSortTransactions,
 } from "../../utils";
 
 import Button from "../Button/Button";
 import DonutChart from "../Charts/DonutChart";
-import { IoIosArrowBack } from "react-icons/io";
 import NoDataCard from "../Cards/NoDataCard";
-import Select from "react-select";
 import TransactionCard from "../Cards/TransactionCard";
 import { getAllTransactions } from "../../db/transactionDb";
 import { useMediaQuery } from "react-responsive";
 import InlineLoader from "../Layouts/InlineLoader";
+import TransactionsMode from "./TransactionsMode";
+import MonthYearSelector from "./ MonthYearSelector";
 
 const SummaryView = ({ title, showMonthSelect = true }) => {
   const isMobile = useMediaQuery({ maxWidth: 768 });
@@ -34,19 +31,16 @@ const SummaryView = ({ title, showMonthSelect = true }) => {
     label: new Date().getFullYear().toString(),
   });
   const [categorySummary, setCategorySummary] = useState([]);
-  const [selectedIndex, setSelectedIndex] = useState(null);
   const [viewMode, setViewMode] = useState("summary");
 
   const [transactions, setTransactions] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [selectedCategoryAmount, setSelectedCategoryAmount] = useState(0);
   const [showSummaryChart, setShowSummaryChart] = useState(true);
-  const [showTransactionChart, setShowTransactionChart] = useState(true);
 
   useEffect(() => {
     if (isMobile) {
       setShowSummaryChart(false);
-      setShowTransactionChart(false);
     }
   }, [isMobile]);
 
@@ -106,35 +100,14 @@ const SummaryView = ({ title, showMonthSelect = true }) => {
     fetchAndSummarize();
   }, [type, month, year, showMonthSelect]);
 
-  const getDonutChartFormatForCategory = (transactions) => {
-    const summaryMap = {};
-    for (let i = 0; i < transactions.length; i++) {
-      const tx = transactions[i];
-      let payee = tx.payee_name || "Others";
-      if (!summaryMap[payee]) {
-        summaryMap[payee] = {
-          amount: 0,
-          payee_name: payee,
-        };
-      }
-      summaryMap[payee].amount += tx.amount;
-    }
 
-    const summaryArray = Object.entries(summaryMap)
-      .map(([payee, data]) => ({
-        name: payee,
-        amount: data.amount,
-        value: Math.round(
-          (data.amount / transactions.reduce((sum, tx) => sum + tx.amount, 0)) *
-          100
-        ),
-      }))
-      .sort((a, b) => b.percentage - a.percentage);
-
-    return summaryArray;
+  const handleBack = () => {
+    setViewMode("summary");
+    setTransactions({});
   };
+
   return (
-    <div>
+    <>
       {viewMode === "summary" && (
         <>
           {/* Toggle Buttons */}
@@ -154,25 +127,14 @@ const SummaryView = ({ title, showMonthSelect = true }) => {
           </div>
 
           {/* Month/Year Selectors */}
-          <div className="filters-wrapper">
-            <Select
-              className="react-select-container"
-              classNamePrefix="react-select"
-              options={getYearOptions()}
-              value={year}
-              onChange={(opt) => setYear(opt)}
-            />
-
-            {showMonthSelect && (
-              <Select
-                className="react-select-container"
-                classNamePrefix="react-select"
-                options={getMonthOptions()}
-                value={month}
-                onChange={(opt) => setMonth(opt)}
-              />
-            )}
-          </div>
+          <MonthYearSelector
+            yearValue={year}
+            onYearChange={(opt) => setYear(opt)}
+            monthValue={month}
+            onMonthChange={(opt) => setMonth(opt)}
+            disabled={loading}
+            showMonth={showMonthSelect}
+          />
 
           {/* Category Summary */}
           <>
@@ -212,7 +174,6 @@ const SummaryView = ({ title, showMonthSelect = true }) => {
                         transaction={category}
                         onCardClick={() => {
                           setViewMode("transactions");
-                          setSelectedIndex(index);
                           setTransactions(
                             groupAndSortTransactions(category.transactions)
                           );
@@ -229,71 +190,14 @@ const SummaryView = ({ title, showMonthSelect = true }) => {
         </>
       )}
       {viewMode === "transactions" && (
-        <>
-          <div
-            className="back-button-container"
-            role="button"
-            tabIndex={0}
-            onClick={() => {
-              setViewMode("summary");
-              setTransactions({});
-              setSelectedIndex(null);
-            }}
-          >
-            <IoIosArrowBack />
-            <span className="back-button">Summary</span>
-          </div>
-
-          <>
-            <div className="align-right">
-              <Button text={
-                isMobile ? null : showTransactionChart ? "Hide Chart" : "Show Chart"
-              } onClick={() => {
-                setShowTransactionChart(!showTransactionChart)
-              }}
-                icon={showTransactionChart ? <FaEyeSlash /> : <FaChartBar />}
-              />
-            </div>
-            {showTransactionChart && (
-              <>
-                {/* Donut Chart */}
-                <DonutChart
-                  data={getDonutChartFormatForCategory(
-                    categorySummary[selectedIndex]?.transactions
-                  )}
-                  colors={getTopCategoryColors(
-                    getDonutChartFormatForCategory(
-                      categorySummary[selectedIndex]?.transactions
-                    ).length
-                  )}
-                />
-              </>
-            )}
-          </>
-
-          <div className="date-summary-bar">
-            <div className="summary-date">{selectedCategory}</div>
-            <div className="summary-amount">
-              {formatIndianNumber(selectedCategoryAmount)}
-            </div>
-          </div>
-          <div className="transaction-page-wrapper">
-            {Object.entries(transactions).map(([date, items]) => (
-              <div key={date} className="transaction-group">
-                <h2 className="transaction-date-header">
-                  {formatDateToDayMonthYear(date)}
-                </h2>
-                <div className="transaction-card-list">
-                  {items.map((tx) => (
-                    <TransactionCard key={tx.id} transaction={tx} />
-                  ))}
-                </div>
-              </div>
-            ))}
-          </div>
-        </>
+        <TransactionsMode
+          name={selectedCategory}
+          amount={selectedCategoryAmount}
+          handleBack={handleBack}
+          transactions={transactions}
+        />
       )}
-    </div>
+    </>
   );
 };
 
