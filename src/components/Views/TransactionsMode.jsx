8bff473142acabad29c4659b53867dcc49bc810d
Jayanthbharadwaj M <jayanthbharadwajm@gmail.com>
2025-07-11 23:46:56 +0530

    fix(TransactionsMode): remove handleBack prop and associated back navigation icon

--- src/components/Views/TransactionsMode.jsx
+++ src/components/Views/TransactionsMode.jsx
@@ -1,16 +1,15 @@
 // src/components/Views/TransactionsMode.jsx
 
-import React from "react";
-import { IoIosArrowBack } from "react-icons/io";
 import { formatDateToDayMonthYear, formatIndianNumber } from "../../utils";
+
+import React from "react";
 import TransactionCard from "../Cards/TransactionCard";
 
-const TransactionsMode = ({ handleBack, name, amount, transactions }) => {
+const TransactionsMode = ({ name, amount, transactions }) => {
   return (
     <>
-      <div className="date-summary-bar" style={{ cursor: 'pointer' }} onClick={handleBack}>
+      <div className="date-summary-bar">
         <div style={{ display: 'flex' }}>
-          <IoIosArrowBack />
           {name ? (
             <div className="summary-date">
               {name}
